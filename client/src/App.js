d08abfa5b31e2f1b9132769d2a96ff61f35515fd
Nikitoo17 <nicolasfadel17@gmail.com>
2023-09-06 17:46:25 -0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Landing from "./views/landing/Landing";
 import Home from "./views/home/Home";
 import Details from "./views/details/Details";
 import FormPage from "./views/form_page/FormPage";
+import axios from "axios";
 
 function App() {
   return (
